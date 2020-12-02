fa862d04baa822efa913ad02340e9fa02b7f0a68
Ashley Smith <38332536+thefad3d@users.noreply.github.com>
2020-12-02 09:42:50 -0800

    Update Signin.slacktoken.sh
    Change to Downloads directory to satisfy requirements per https://slack.com/intl/en-au/help/articles/360041725993-Share-a-default-sign-in-file-with-members

--- Signin.slacktoken.sh
+++ Signin.slacktoken.sh
@@ -7,7 +7,7 @@
 
 USERNAME=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 
-SLACKPATH="/Users/$USERNAME/Library/Application Support/Slack"
+SLACKPATH="/Users/$USERNAME/Downloads"
 
 if [ ! -e "$SLACKPATH" ]; then
  	mkdir -p "$SLACKPATH"
